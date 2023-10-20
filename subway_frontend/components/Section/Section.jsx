cc43291f4060f3bd274f283e6521035a89022d5c
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-10-20 14:47:07 +0300

    added localization for article pages

--- subway_frontend/components/Section/Section.jsx
+++ subway_frontend/components/Section/Section.jsx
@@ -3,7 +3,7 @@ import css from "./Section.module.css"
 
 export const Section = (props) => {
   return(
-    <div className={css.main_section}>
+    <div className={css.main_section} key={props.src}>
         <p className={css.art_text}>{props.text}</p>
         <img className={css.image} src={props.src} />
         <p className={css.caption}>{props.caption}</p>
---
+++ subway_frontend/pages/Stations/[id]/[locale].jsx
@@ -0,0 +1,73 @@
+import { Section } from "@/components/Section/Section";
+import { useRouter } from "next/router";
+import { Head } from "@/components/Head/Head";
+import React, { useEffect, useState } from "react";
+import css from "../index.module.css"
+import { compile } from "vue";
+
+const StationItem = ({data, data_st}) => {
+    const router = useRouter();
+    let [stName, setStName] = useState("")
+    let [head, setHead] = useState([])
+    console.log(data_st)
+    useEffect(() => {
+            if (router.query.locale == "en"){
+                setStName(data_st.nameEn + " station")
+            }
+            if (router.query.locale == "ru"){
+                setStName("Станция " + data_st.name)
+            }
+        }, []
+    )
+
+    useEffect(() => {
+        if (router.query.locale == "en"){
+            setHead(["Main", "Stations"])
+        }
+        if (router.query.locale == "ru"){
+            setHead(["Главная", "Станции"])
+        }
+    }, []
+)
+        console.log(router.query.locale);
+        return <div>
+            <Head main = {head[0]} stations = {head[1]}></Head>
+            <div className={css.article_pan}>
+                <h2 className={css.title_article}>{stName}</h2>
+                {data.map(d => (
+                    <Section text = {d.text} src = {d.src} caption = {d.caption}/>
+                ))}
+            </div>
+        </div>;
+}
+
+export default StationItem;
+
+export async function getStaticProps(context) {
+    const st_article = await fetch(`http://localhost:8080/student/getArticle/${context.params.id}/${context.params.locale}`).then(res=>res.json())
+    const station = await fetch(`http://localhost:8080/student/getStation/${context.params.id}`).then(res=>res.json())
+    const locs = context.params.locale
+    return {
+        props: {
+            data: st_article,
+            data_st: station,
+            loc: locs,
+        },
+    };
+}
+
+export async function getStaticPaths(context) {
+    return {
+        paths: [
+            { params: {id: "1", locale: "ru"}},
+            { params: {id: "2", locale: "ru"}},
+            { params: {id: "3", locale: "ru"}},
+            { params: {id: "4", locale: "ru"}},
+        
+            { params: {id: "1", locale: "en"}},
+            { params: {id: "2", locale: "en"}},
+            { params: {id: "3", locale: "en"}},
+            { params: {id: "4", locale: "en"}}],
+        fallback: false,
+    };
+}
