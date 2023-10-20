499b0d47cce7f57722b4e69e8a7a19a47acbb4c1
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-10-20 20:09:45 +0300

    added localization button beta

--- subway_frontend/pages/Stations/[id]/[locale].jsx
+++ subway_frontend/pages/Stations/[id]/[locale].jsx
@@ -4,12 +4,13 @@ import { Head } from "@/components/Head/Head";
 import React, { useEffect, useState } from "react";
 import css from "../index.module.css"
 import { compile } from "vue";
+import Link from "next/link";
 
 const StationItem = ({data, data_st}) => {
     const router = useRouter();
     let [stName, setStName] = useState("")
     let [head, setHead] = useState([])
-    console.log(data_st)
+    let [loch, setLoch] = useState("")
     useEffect(() => {
             if (router.query.locale == "en"){
                 setStName(data_st.nameEn + " station")
@@ -17,22 +18,37 @@ const StationItem = ({data, data_st}) => {
             if (router.query.locale == "ru"){
                 setStName("Станция " + data_st.name)
             }
-        }, []
+        }, router.query.locale, router.query.id
     )
 
     useEffect(() => {
         if (router.query.locale == "en"){
             setHead(["Main", "Stations", "en"])
+            setLoch(`${router.query.id}/ru`)
         }
         if (router.query.locale == "ru"){
             setHead(["Главная", "Станции", "ru"])
+            setLoch(`${router.query.id}/en`)
         }
-    }, []
-)
+        
+    }, router.query.locale
+    )
+
+    // useEffect(() => {
+    //     if (router.query.locale == "en"){
+    //         setLoch([`/Stations/${router.query.id}/ru`, "Русский"])
+    //     }
+    //     if (router.query.locale == "ru"){
+    //         setLoch([`/Stations/${router.query.id}/en`, "Английский"])
+    //     }
+    //     console.log(loch[1])
+    // }, []
+    // )
         console.log(router.query.locale);
         return <div>
             <Head main = {head[0]} stations = {head[1]} local = {head[2]}></Head>
             <div className={css.article_pan}>
+            <Link href = {`/Stations/${loch}`}>qqq</Link>
                 <h2 className={css.title_article}>{stName}</h2>
                 {data.map(d => (
                     <Section text = {d.text} src = {d.src} caption = {d.caption}/>
