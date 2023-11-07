40f72332224ed8fbf36e885e393e8bf6728d00b2
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-11-07 22:27:19 +0300

    added base styles

--- subway_frontend/components/StCard/StCard.jsx
+++ subway_frontend/components/StCard/StCard.jsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 export const StCard = (props) => {
   return(
     <div className={css.main_section} key={props.src}>
-        <div>
+        <div className={css.photo_list}>
             <ul>
                 <li><img className={css.small_img} src={props.img1} /></li>
                 <li><img className={css.small_img} src={props.img2}/></li>
                 <li><img className={css.small_img} src={props.img3}/></li>
             </ul>
         </div>
-        <div>
+        <div className={css.info}>
             <p>{props.name}</p>
             <p>{props.line}</p>
             <p>{props.year}</p>
--- subway_frontend/pages/AllStations/[locale].jsx
+++ subway_frontend/pages/AllStations/[locale].jsx
@@ -6,7 +6,7 @@ import { Head } from "@/components/Head/Head";
 import { StCard } from "@/components/StCard/StCard";
 import css from "./index.module.css"
 
-const LocaleIndex = ({data}) => {
+const LocaleIndex = ({data, l_data}) => {
     const router = useRouter();
     let [head, setHead] = useState([])
     let [loc, setLoc] = useState("")
@@ -25,8 +25,8 @@ const LocaleIndex = ({data}) => {
         <Head main = {head[0]} stations = {head[1]} local = {head[2]} hr = "/AllStations"></Head>
         <div className={css.linkPan}>
             {data.map(s => (
-                <div key={s.id}>
-                    <StCard img1 = {s.img1} img2 = {s.img2} img3 = {s.img3} linka = {`/Stations/${s.id}/${head[2]}`} year = {s.year} name = {s.name} ></StCard>
+                <div key={s.id} className={css.cards}>
+                    <StCard img1 = {s.img1} img2 = {s.img2} img3 = {s.img3} linka = {`/Stations/${s.id}/${head[2]}`} year = {s.year} name = {s.name} line = {l_data[s.line_id - 1].name}></StCard>
                     {/* <p><Link href={`/Stations/${s.id}/${head[2]}`}>{s.name}</Link></p> */}
                 </div>
             ))}
@@ -44,6 +44,7 @@ export async function getStaticProps(context) {
     return {
         props: {
             data: stations,
+            l_data: lines,
         },
     };
 }
