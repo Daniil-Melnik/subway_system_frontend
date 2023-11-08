cc049310842aba64c7b051d3b85e191f3c6b9b7a
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-11-08 23:34:19 +0300

    added changing between table and list of cards

--- subway_frontend/pages/AllStations/[locale].jsx
+++ subway_frontend/pages/AllStations/[locale].jsx
@@ -10,10 +10,13 @@ const LocaleIndex = ({data, l_data}) => {
     const router = useRouter();
     let [head, setHead] = useState([])
     let [loc, setLoc] = useState("")
+    let [liTab, setLiTab] = useState(1)
     let [dataM, setDataM] = useState(data)
     let [stName, setStName] = useState("")
+    let [btnTxt, setBtnTxt] = useState("Список")
     useEffect(() => {
         let k = []
+        console.log(data)
             if (router.query.locale == "en"){
                 setHead(["Main", "Stations", "en"])
             }
@@ -21,30 +24,67 @@ const LocaleIndex = ({data, l_data}) => {
                 setHead(["Главная", "Станции", "ru"])
             }
             for (let i = 0; i < data.length; i++){
-                if (data[i].name.toUpperCase().includes(stName.toUpperCase())){
-                    k.push(data[i])
-                }
+                    if (data[i].name.toUpperCase().includes(stName.toUpperCase())){
+                        k.push(data[i])
+                    }
             }
             setDataM(k)
         }, [router.query.locale, stName]
     )
+    const handleClick_1=(e)=>{
+        setLiTab(0)
+        setBtnTxt("Таблица")
+    }
+    const handleClick_2=(e)=>{
+        setLiTab(1)
+        setBtnTxt("Список")
+    }
 
-  return (
-    <div>
-        <Head main = {head[0]} stations = {head[1]} local = {head[2]} hr = "/AllStations"></Head>
-        <p className={css.main_title}>Станции</p>
-        <input type="text" label="Название станции" value={stName} onChange={(e)=>setStName(e.target.value)} placeholder="Название станции" className={css.input}/>
-        <div className={css.linkPan}>
-            {dataM.map(s => (
-                <div key={s.id} className={css.cards}>
-                    <StCard img1 = {s.img1} img2 = {s.img2} img3 = {s.img3} linka = {`/Stations/${s.id}/${head[2]}`} year = {s.year} name = {s.name} line = {l_data[s.line_id - 1].name} line_id = {s.line_id}></StCard>
-                    {/* <p><Link href={`/Stations/${s.id}/${head[2]}`}>{s.name}</Link></p> */}
+    if (liTab == 1){
+        return (
+            <div>
+                <Head main = {head[0]} stations = {head[1]} local = {head[2]} hr = "/AllStations"></Head>
+                <button className={css.btn} onClick={handleClick_1}>{btnTxt}</button>
+                <p className={css.main_title}>Станции</p>
+                <input type="text" label="Название станции" value={stName} onChange={(e)=>setStName(e.target.value)} placeholder="Название станции" className={css.input}/>
+                <div className={css.linkPan}>
+                    {dataM.map(s => (
+                        <div key={s.id} className={css.cards}>
+                            <StCard img1 = {s.img1} img2 = {s.img2} img3 = {s.img3} linka = {`/Stations/${s.id}/${head[2]}`} year = {s.year} name = {s.name} line = {l_data[s.line_id - 1].name} line_id = {s.line_id}></StCard>
+                        </div>
+                    ))}
                 </div>
-            ))}
-        </div>
-        
-    </div>
-  );
+            </div>
+        );
+    }
+    if (liTab == 0){
+        return (
+            <div>
+                <Head main = {head[0]} stations = {head[1]} local = {head[2]} hr = "/AllStations"></Head>
+                <button className={css.btn} onClick={handleClick_2}>{btnTxt}</button>
+                <p className={css.main_title}>Станции</p>
+                <input type="text" label="Название станции" value={stName} onChange={(e)=>setStName(e.target.value)} placeholder="Название станции" className={css.input}/>
+                <div className={css.linkPan}>
+                    <table>
+                        <tr>
+                            <th>Название</th>
+                            <th>Год</th>
+                            <th>Линия</th>
+                            <th>№ линии</th>
+                        </tr>
+                        {dataM.map(s => (
+                            <tr>
+                                <td><Link href={`/Stations/${s.id}/${head[2]}`}>{s.name}</Link></td>
+                                <td>{s.year}</td>
+                                <td>{l_data[s.line_id - 1].name}</td>
+                                <td>{s.line_id}</td>
+                            </tr>
+                        ))}
+                    </table>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default LocaleIndex;
