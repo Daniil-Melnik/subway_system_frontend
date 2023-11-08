c3e42763720cee2cb33f7b76b7d0a2b61071d131
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-11-08 22:00:11 +0300

    added search to stations panel

--- subway_frontend/pages/AllStations/[locale].jsx
+++ subway_frontend/pages/AllStations/[locale].jsx
@@ -10,21 +10,32 @@ const LocaleIndex = ({data, l_data}) => {
     const router = useRouter();
     let [head, setHead] = useState([])
     let [loc, setLoc] = useState("")
+    let [dataM, setDataM] = useState(data)
+    let [stName, setStName] = useState("")
     useEffect(() => {
+        let k = []
             if (router.query.locale == "en"){
                 setHead(["Main", "Stations", "en"])
             }
             if (router.query.locale == "ru"){
                 setHead(["Главная", "Станции", "ru"])
             }
-        }, router.query.locale
+            for (let i = 0; i < data.length; i++){
+                if (data[i].name.toUpperCase().includes(stName.toUpperCase())){
+                    k.push(data[i])
+                }
+            }
+            setDataM(k)
+        }, [router.query.locale, stName]
     )
 
   return (
     <div>
         <Head main = {head[0]} stations = {head[1]} local = {head[2]} hr = "/AllStations"></Head>
+        <p className={css.main_title}>Станции</p>
+        <input type="text" label="Название станции" value={stName} onChange={(e)=>setStName(e.target.value)} placeholder="Название станции" className={css.input}/>
         <div className={css.linkPan}>
-            {data.map(s => (
+            {dataM.map(s => (
                 <div key={s.id} className={css.cards}>
                     <StCard img1 = {s.img1} img2 = {s.img2} img3 = {s.img3} linka = {`/Stations/${s.id}/${head[2]}`} year = {s.year} name = {s.name} line = {l_data[s.line_id - 1].name} line_id = {s.line_id}></StCard>
                     {/* <p><Link href={`/Stations/${s.id}/${head[2]}`}>{s.name}</Link></p> */}
